bb10f6e5c55842d5989877e3d1475c433e5ad614
Jamie Bort <jamiebort@gmail.com>
2023-11-07 11:33:29 -0500

    Control 'Add Todo' input.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -5,7 +5,6 @@ import React, { useState } from "react";
 export default function AddTodoForm({ onAddTodo }) {
   const [todoTitle, setTodoTitle] = useState("");
   const handleTitleChange = (event) => setTodoTitle(event.target.value);
-
   const handleAddTodo = (event) => {
     event.preventDefault();
     // onAddTodo({ title: todoTitle, id: Date.now() });
