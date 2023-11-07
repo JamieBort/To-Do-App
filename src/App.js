8bb145e79b19ae51a7ee275d743afb06b8288432
Jamie Bort <jamiebort@gmail.com>
2023-11-07 13:05:13 -0500

    Create custom hook.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 // ./src/App.js
-import React, { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+const useSemiPersistentState = () => {
+  const storageKey = "savedTodoList";
+  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem(storageKey)) ?? []);
+
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+};
+
 export default function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
+
   const addTodo = (newTodo) => setTodoList([...todoList, newTodo]);
 
   return (
-    <div>
+    <>
       <h1>Todo List</h1>
+      <p>For the future: add and delete buttons to create lists. Field to name the list.</p>
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
